0de1cd3329f19ff49d24cdb53ea0cd4529bb020f
KingNarmar <adlymina99@gmail.com>
2024-08-01 13:30:22 +0300

    Video 63 Change Drawer item State

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
-
 import 'package:responsive_dash_board/utils/app_images.dart';
-import 'package:responsive_dash_board/widgets/drawer_item.dart';
 import 'package:responsive_dash_board/widgets/user_info_list_tile.dart';
-
-import '../models/drawer_item_model.dart';
+import 'drawer_item_list_view.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
-  static List<DrawerItemModel> items = [
-    DrawerItemModel(image: Assets.imagesDashboard, title: "Dashboard"),
-    DrawerItemModel(image: Assets.imagesMyTransctions, title: "My Transaction"),
-    DrawerItemModel(image: Assets.imagesStatistics, title: "Statistics"),
-    DrawerItemModel(image: Assets.imagesWalletAccount, title: "Wallet Account"),
-    DrawerItemModel(image: Assets.imagesMyInvestments, title: "My Investments"),
-  ];
+
   @override
   Widget build(BuildContext context) {
     final double height = MediaQuery.of(context).size.height;
@@ -30,19 +21,10 @@ class CustomDrawer extends StatelessWidget {
           SizedBox(
             height: height * 0.00814664,
           ),
-          ListView.builder(
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: DrawerItem(drawerItemModel: items[index]),
-              );
-            },
-          )
+          const DrawerItemListView()
         ],
       ),
     );
   }
 }
+
---
+++ lib/widgets/drawer_item_list_view.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/drawer_item.dart';
+
+import '../models/drawer_item_model.dart';
+import '../utils/app_images.dart';
+
+class DrawerItemListView extends StatefulWidget {
+  const DrawerItemListView({
+    super.key,
+  });
+
+  @override
+  State<DrawerItemListView> createState() => _DrawerItemListViewState();
+}
+
+class _DrawerItemListViewState extends State<DrawerItemListView> {
+  final List<DrawerItemModel> items = [
+    DrawerItemModel(image: Assets.imagesDashboard, title: "Dashboard"),
+    DrawerItemModel(image: Assets.imagesMyTransctions, title: "My Transaction"),
+    DrawerItemModel(image: Assets.imagesStatistics, title: "Statistics"),
+    DrawerItemModel(image: Assets.imagesWalletAccount, title: "Wallet Account"),
+    DrawerItemModel(image: Assets.imagesMyInvestments, title: "My Investments"),
+  ];
+  int activeIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () {
+            if (activeIndex != index) {
+              setState(() {
+                activeIndex = index;
+                print(activeIndex);
+              });
+            }
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: DrawerItem(
+              drawerItemModel: items[index],
+              isActive: activeIndex == index,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
